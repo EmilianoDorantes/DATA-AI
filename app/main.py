4ac9a025bbc01572d2696bdbc11076ef8f47c8ea
EmilianoDorantes <emiliano.dorantes@grupodelcen.com>
2025-06-16 13:16:03 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.core.config.config import create_agent_instance
+from app.core.config.config import create_agent_instance, create_agent_instance_with_path
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-agent = create_agent_instance()
-
-
 @app.get("/chat")
-async def get_my_chat(prompt: str):
+async def get_my_chat(prompt: str, data_url: str = Query(None)):
+    if data_url:
+        agent = create_agent_instance_with_path(data_url)
+    else:
+        agent = create_agent_instance()
     return agent.chat(prompt)
